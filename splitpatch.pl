afa12d708ad81dfa4f6d4c81b8c361d4cc9c84eb
Georgios Koloventzos <sfagmenos@users.noreply.github.com>
2015-11-06 21:45:57 -0500

    start the search in file

--- splitpatch.pl
+++ splitpatch.pl
@@ -7,11 +7,25 @@ use Getopt::Long;
 
 my $help = 0;
 my $file = "";
+my $verbose = 0;
+
+if ($#ARGV == 0 and ($ARGV[0] =~ m/-h/)) {
+	print "Usage: splitpatch.pl patch_file\n";
+	print "-file|f <list of files>   list of files you want to create patches\n";
+	print "-help|?			 print this message\n";
+	exit 0;
+}
 
 if ( @ARGV > 0 ) {
 	    GetOptions('verbose'	=> \$verbose,
-		       'trace'	=> \$trace,
 		       'help|?'	=> \$help,
-		       'manual'	=> \$man,
-		       'debug'	=> \$debug);
+		       "file=s" => \@file,);
 }
+
+my @lines = split /\n/, $ARGV[0];
+
+foreach my $line (@lines) {
+	
+}
+
+exit 0;
